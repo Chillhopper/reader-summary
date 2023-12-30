5eb18d98cd8ef95d3745d9b8071481ebb32c8157
Chillhopper <68851163+Chillhopper@users.noreply.github.com>
2023-12-30 18:04:22 +0800

    nodejs backend Receptive
    However the parsing of ALL of its contents need work

--- my-react-app/src/assets/components/Reader.tsx
+++ my-react-app/src/assets/components/Reader.tsx
@@ -5,16 +5,29 @@ const fileReader = (files: FileList) => {
   for (let i = 0; i < files.length; i++) {
     const file = files[i];
 
-    // Now you can process each file. For example, you can read them as text.
+
     const reader = new FileReader();
     reader.onload = (e) => {
       // e.target.result contains the file's content
-      console.log(`Content of file ${file.name}:`, e.target.result);
+      const formData = new FormData();
+      formData.append('pdfFile', file);
+      console.log(`Content of file ${file.name}:`, e.target?.result);
+
+      fetch('http://localhost:3001/submit-data', {
+        method: 'POST',
+        // headers: {
+        //   'Content-Type': 'text/plain',
+        // },
+         body: formData,//JSON.stringify({ fileData: e.target?.result }),
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch((error) => console.error('Error:', error));
     };
     reader.onerror = (e) => {
-      console.error(`Error reading file ${file.name}:`, e.target.error);
+      console.error(`Error reading file ${file.name}:`, e.target?.error);
     };
-    reader.readAsText(file); // Or use readAsDataURL(), readAsArrayBuffer(), etc., depending on your needs
+    reader.readAsText(file);
   }
 };
 
--- server/server.js
+++ server/server.js
@@ -1,14 +1,47 @@
 const express = require("express");
+const fs = require('fs');
+const pdf = require('pdf-parse');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+app.use(cors());
+app.use(fileUpload());
+
+//end point
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
+// app.post("/submit-data", (req, res) => {
+//     const data = req.body; 
+//     console.log("Received data:", data);
+    
   
+//     res.json({ message: "Data received successfully" });
+//   });
+
+  app.post("/submit-data", (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+  
+    // Get the uploaded file
+    const pdfFile = req.files.pdfFile; // 'pdfFile' should match the name attribute in your frontend file input
+  
+    // Use pdf-parse to extract text from the uploaded file
+    pdf(pdfFile.data)
+      .then(data => {
+        console.log("PDF Text:", data.text); // Log the text content of the PDF
+        res.json({ message: "PDF processed successfully", text: data.text });
+      })
+      .catch(error => {
+        console.error("Error processing PDF:", error);
+        res.status(500).send("Error processing PDF");
+      });
+  });
